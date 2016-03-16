251dc7b27628e396d359d612a3f6ad536761771b
Michael Delgado Beramendi <Michael Delgado Beramendi>
2016-03-16 18:07:28 +0100

    Añadido el primer método

--- Principal.java
+++ Principal.java
@@ -12,7 +12,9 @@
 	}
 //  metodos
               1
-              2
+            public void llena(){
+				this.cantidad=this.capacidad;
+			}
               3
 
         public void metodoComunitario(){ 
